9f0b3dcdc84bf0a62f46df37bdfefc979930117e
Jonathan Straub <jstronix5150@gmail.com>
2022-04-19 00:40:46 -0500

    Local setup and colemak
    * Setup IO for my handwire version of Redox.
    * Add F11, F12, Break, Insert keys.
    * Add Colemak base layer [1].
    * Add base layer switching keys to adjust layer.

--- keyboards/redox/rev1/config.h
+++ keyboards/redox/rev1/config.h
@@ -19,12 +19,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 // wiring of each half
-#define MATRIX_ROW_PINS { D4, D7, E6, B4, B5 }
-#define MATRIX_COL_PINS { F5, F6, F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS { D1, D0, D4, C6, D7 }
+//#define MATRIX_ROW_PINS { D4, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { E6, B4, B5, B6, B2, B3, B1 }
+//#define MATRIX_COL_PINS { F5, F6, F7, B1, B3, B2, B6 }
 // #define MATRIX_COL_PINS { B6, B2, B3, B1, F7, F6, F5} //uncomment this line and comment line above if you need to reverse left-to-right key order
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
+
+/* define if matrix has ghost */
+//#define MATRIX_HAS_GHOST
+
+/* number of backlight levels */
+// #define BACKLIGHT_LEVELS 3
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* serial.c configuration for split keyboard */
+#define SOFT_SERIAL_PIN D3
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
@@ -32,7 +46,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LOCKING_RESYNC_ENABLE
 
 /* ws2812 RGB LED */
-#define RGB_DI_PIN D3
+#define RGB_DI_PIN F4
 
 #define RGBLED_NUM 14    // Number of LEDs
 
